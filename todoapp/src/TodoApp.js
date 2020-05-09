ef33fa3b8ba9af6d37d45a5c3dac95c2f02db8d8
abhishek.g <abhishek.gapd@gmail.com>
2020-05-09 14:53:02 +0530

    Todo App with redux example

---
+++ todoapp/src/TodoApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Text} from 'react-native'
+export default class TodoApp extends Component {
+
+constructor(props){
+super(props)
+}
+
+render(){
+    return (
+        <Text>HI</Text>
+    );
+}
+}
